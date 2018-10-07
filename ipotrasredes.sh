ded645cbd73f7df56294774ef3444a987a9cd5a7
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-10-08 00:15:40 +0200

    Update ipotrasredes.sh

--- ipotrasredes.sh
+++ ipotrasredes.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+ip addr add 10.0.0.1/8 dev eth0 label eth0:1
+ip addr add 172.16.0.1/12 dev eth0 label eth0:2
+
+#Cambiar gateway 
+#route add default gw 10.0.0.1 eth0
+#route add default gw 192.168.1.1 eth0
