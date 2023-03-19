086c023e103eb1229e200d31a92f461cf6e4d397
hackingyseguridad <antonio_taboada@hotmail.es>
2023-03-19 12:50:12 +0100

    Create conectawifi.sh

---
+++ conectawifi.sh
@@ -0,0 +1,10 @@
+
+iwconfig
+ifconfig
+ifconfig wlan0 up
+ifup wlan0
+iwlist wlan0 scanning
+nmcli device wifi
+nmcli d wifi connect hackingyseguridad password 12345 iface wlan0
+dhclient wlan0
+iwconfig wlan0
