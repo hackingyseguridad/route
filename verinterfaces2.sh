4568317174fc1b4f601d349b4f187742e896ad77
hackingyseguridad <antonio_taboada@hotmail.es>
2025-06-15 20:39:25 +0200

    Create verinterfaces2.sh

---
+++ verinterfaces2.sh
@@ -0,0 +1,15 @@
+
+echo "-----------------------------"
+ip link show
+echo "-----------------------------"
+ifconfig -a
+echo "-----------------------------"
+sudo ip link set eth1 up
+sudo ip link set eth2 up
+sudo ip link set eth3 up
+sudo ip link set wlan0 up
+echo "-----------------------------"
+sudo dhclient eth1
+sudo dhclient eth2
+sudo dhclient eth3
+echo "-----------------------------"
